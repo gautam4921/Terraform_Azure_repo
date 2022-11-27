e8c8d99393e664ed8a3a8924a675f931626b2a88
gautam4921 <45917285+gautam4921@users.noreply.github.com>
2022-11-27 22:39:52 +0530

    c

---
+++ tfvars.tf
@@ -0,0 +1,3 @@
+
+aws_access_key = "AKIAUSGG6HHLGNGUSH57"
+aws_secret_key = "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"
---
+++ vars.tf
@@ -0,0 +1,7 @@
+variable "aws_access_key" {}
+
+variable "aws_secret_key" {}
+
+variable "region" {
+        default = "us-east-2"
+}
